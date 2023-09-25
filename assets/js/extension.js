4179396b6e5d25a629cd42fa63d1e8275ea9a277
Bùi H Quân <bhquaninit27@gmail.com>
2023-09-25 15:48:16 +0700

    fix responsive gg map

--- assets/js/extension.js
+++ assets/js/extension.js
@@ -1,26 +1,26 @@
 // menu mobile
 $(function () {
-    $("#icon-healthy").draggable();
-    $(".close-icon").click(function () {
-      $("#icon-healthy").hide();
-    });
+  $("#icon-healthy").draggable();
+  $(".close-icon").click(function () {
+    $("#icon-healthy").hide();
   });
+});
 
-$(document).ready(function() {
-    $('.sidebarMenu .SMABody ul li a').click(function(e) {
-      e.preventDefault();
-      var target = $(this).attr('href')
-      $('.sidebarMenu li').removeClass('active');
-      $(this).addClass('active');
-      $('html, body').animate({
-        scrollTop: $(target).offset().top
-      }, 800);
-    });
+$(document).ready(function () {
+  $('.sidebarMenu .SMABody ul li a').click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr('href')
+    $('.sidebarMenu li').removeClass('active');
+    $(this).addClass('active');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 800);
   });
+});
 
-  // menu PC
-$(document).ready(function() {
-  $('ul li a').click(function(e) {
+// menu PC
+$(document).ready(function () {
+  $('ul li a').click(function (e) {
     e.preventDefault();
     var target = $(this).attr('href');
     $('html, body').animate({
@@ -30,59 +30,62 @@ $(document).ready(function() {
 });
 
 // open pop-up read news
-$(document).ready(function() {
-    $('.overlay').click(function() {
-      hidePopup();
-    });
-  
-    function showPopup(popupId) {
-      $(popupId).addClass('show');
-      $('.overlay').addClass('show');
-    }
-  
-    function hidePopup() {
-      $('.popup').removeClass('show');
-      $('.overlay').removeClass('show');
-    }
-  
-    $('.blog1').click(function() {
-        showPopup('#popup1');
-      });
-    
-      $('.blog2').click(function() {
-        showPopup('#popup2');
-      });
-    
-      $('.blog3').click(function() {
-        showPopup('#popup3');
-      });
-      $('.blog4').click(function() {
-        showPopup('#popup4');
-      });
-      $('.blog5-header').click(function() {
-        showPopup('#popup4');
-      });
+$(document).ready(function () {
+  $('.overlay').click(function () {
+    hidePopup();
   });
 
-  // update img for Mobile
-  function updateImageSrc() {
-    var screenWidth = window.innerWidth;
-    var images = document.querySelectorAll('.rev-slidebg');
-    
-    images.forEach(function(image) {
-        var mobileSrc = image.getAttribute('data-mobile-src');
-        var desktopSrc = image.getAttribute('data-desktop-tablet-src');
-        
-        if (screenWidth < 768 && mobileSrc) {
-            image.src = mobileSrc;
-        } else if (desktopSrc) {
-            image.src = desktopSrc;
-        }
-    });
+  $('.close-popup-icon').click(function () {
+    hidePopup();
+  });
+
+  function showPopup(popupId) {
+    $(popupId).addClass('show');
+    $('.overlay').addClass('show');
+  }
+
+  function hidePopup() {
+    $('.popup').removeClass('show');
+    $('.overlay').removeClass('show');
+  }
+
+  $('.blog1').click(function () {
+    showPopup('#popup1');
+  });
+
+  $('.blog2').click(function () {
+    showPopup('#popup2');
+  });
+
+  $('.blog3').click(function () {
+    showPopup('#popup3');
+  });
+  $('.blog4').click(function () {
+    showPopup('#popup4');
+  });
+  $('.blog5-header').click(function () {
+    showPopup('#popup4');
+  });
+});
+
+// update img for Mobile
+function updateImageSrc() {
+  var screenWidth = window.innerWidth;
+  var images = document.querySelectorAll('.rev-slidebg');
+
+  images.forEach(function (image) {
+    var mobileSrc = image.getAttribute('data-mobile-src');
+    var desktopSrc = image.getAttribute('data-desktop-tablet-src');
+
+    if (screenWidth < 768 && mobileSrc) {
+      image.src = mobileSrc;
+    } else if (desktopSrc) {
+      image.src = desktopSrc;
+    }
+  });
 }
 window.addEventListener('resize', updateImageSrc);
 updateImageSrc();
 
 
 
-  
--- assets/js/gg-map.js
+++ assets/js/gg-map.js
@@ -4,7 +4,7 @@ var listDisGGmap = [
         tench: "MD Coffee", ten: "Số 2 B11 đường Hàm Nghi, Phường Cầu Diễn, Quận Nam Từ Liêm, Hà Nội",
         diachi: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.956702173258!2d105.76559867504878!3d21.034418387584587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b9bca289c3%3A0x66d7b5fec5199ca4!2zMiBQLiBIw6BtIE5naGksIE3hu7kgxJDDrG5oLCBOYW0gVOG7qyBMacOqbSwgSMOgIE7hu5lpIDEwMDAwMCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1695179001609!5m2!1svi!2s",
         khuvuc: "hanoi",
-        zip:"100000",
+        zip: "100000",
         sdt: "0981 881 369"
     },
     {
@@ -13,36 +13,41 @@ var listDisGGmap = [
         ten: "33 Xuân Tảo, Xuân La, Tây Hồ, Hà Nội",
         diachi: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.535542016421!2d105.79478557504912!3d21.05126228700573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abc5acdd15ad%3A0x8e2f0ee8080b7fd7!2zxJBpIENhZmXigKY!5e0!3m2!1svi!2s!4v1695179088153!5m2!1svi!2s",
         khuvuc: "hanoi",
-        zip:"100000",
+        zip: "100000",
         sdt: "0388 001 369"
     },
     {
         id: 2,
-        tench: "NHƯ Ý COFFEE",
+        tench: "NHƯ Ý coffee",
         ten: "Như Ý coffee, 102A Chung cư 7 tầng lô C3 Xuân Lộc 4 Xuân Đỉnh, Bắc Từ Liêm, Hà Nội",
         diachi: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14892.391165164476!2d105.77693914846938!3d21.06875608561003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0935cea279%3A0x38cc8d398cbf7986!2zTmjGsCDDnSBDb2ZmZWU!5e0!3m2!1svi!2s!4v1695179152882!5m2!1svi!2s",
         khuvuc: "hanoi",
-        zip:"100000",
+        zip: "100000",
         sdt: "0979 390 174"
-    },  
+    }
 ];
 
 var tacccElements = document.querySelectorAll(".taccc");
 var listMap = document.querySelector(".no-bullets");
 var mapElement = document.getElementById("map");
+var divElement = document.querySelector(".drop_list-item");
+const toggleButton = document.getElementById("toggleButton");
+let ulElement;
+let ulVisible = false;
 var selectedIndex = -1;
 var searchResults = [];
 
-function render(listDisGGmap) { 
+
+function render(listDisGGmap) {
     const htmls = listDisGGmap.map((dt, index) => {
-        const isSelected = selectedIndex === index ? "selected" : "";
+        const isSelected = selectedIndex === index || index === 0 ? "selected" : "";
         return `
             <li onclick="showInfo(${index})" data-index="${index}" data-value="${dt.khuvuc}" class="taccc ${isSelected}">
-            <p>Tên cửa hàng: ${dt.tench}</p>
-            <p class="datafilterdistrict" data-filter="${dt.ten}" style="margin-bottom: 0px;">
+            <p class="shop" style="margin: 0 0 -2px 0; font-size:1.2rem;color:black;font-weight:500;">${dt.tench}</p>
+            <p class="datafilterdistrict" data-filter="${dt.ten}" style="margin-bottom: 0px; color:black">
             <i class="twi-location-arrow1" aria-hidden="true" style="margin-right: 5px;display: unset;font-weight: bold;"></i>${dt.ten}
             </p>
-            <a href="tel:${dt.sdt}"><i class="twi-phone" aria-hidden="true" style="margin-right: 4px;display: unset;font-weight: bold;color:#959595;"></i>${dt.sdt}</a>
+            <a href="tel:${dt.sdt}"><i class="twi-phone" aria-hidden="true" style="margin-right: 4px;display: unset;font-weight: bold;color:black;"></i>${dt.sdt}</a>
             </li>
         `;
     });
@@ -52,15 +57,24 @@ function render(listDisGGmap) {
 const showInfo = (index) => {
     selectedIndex = index;
 
-    const allLiElements = document.querySelectorAll(".taccc");
-    allLiElements.forEach((li) => {
-        li.classList.remove("selected");
-    });
+    removeSelected();
     const selectedLi = document.querySelector(`[data-index="${selectedIndex}"]`);
     selectedLi.classList.add("selected");
     const mapSource = searchResults.length > 0 ? searchResults[index].diachi : listDisGGmap[index].diachi;
     mapElement.src = mapSource;
-    
+    const nameShop = document.querySelectorAll('.shop');
+    const n = document.querySelector('.name-shop');
+    console.log(nameShop[index].innerHTML);
+    n.innerHTML = nameShop[index].innerHTML;
+}
+
+function removeSelected() {
+    const allLiElements = document.querySelectorAll(".taccc");
+    allLiElements.forEach((li) => {
+        li.classList.remove("selected");
+        divElement.style.display = "none";
+        ulVisible = false;
+    });
 }
 
 
@@ -83,7 +97,7 @@ function searchShops(keyword, province) {
             newSearchResults.push(shop);
         }
     }
-    
+
     if (newSearchResults.length > 0) {
         searchResults = newSearchResults;
         mapElement.src = searchResults[0].diachi;
@@ -91,15 +105,51 @@ function searchShops(keyword, province) {
         searchResults = [];
         mapElement.src = '';
     }
-    
+
     render(searchResults);
 }
-
-
 
 loadShops = () => {
     const keyword = document.getElementById('shops-name').value;
     const province = document.getElementById('shops-province').value;
-    searchShops(keyword,province)
+    const allLiElements = document.querySelectorAll(".taccc");
 
+    allLiElements.forEach((li) => {
+        if (li.classList.contains("selected") && li.dataset.index !== selectedIndex.toString()) {
+            li.classList.remove("selected");
+        }
+    });
+    searchShops(keyword, province)
 }
+
+toggleButton.addEventListener("click", function () {
+    if (!ulElement) {
+        ulElement = listMap;
+        divElement.appendChild(ulElement);
+    }
+    if (ulVisible) {
+        divElement.style.display = "none";
+        ulVisible = false;
+    } else {
+        divElement.style.display = "block";
+        ulVisible = true;
+    }
+});
+
+// function toggleDropList() {
+//     const liElements = divElement.querySelectorAll(".taccc");
+//     // liElements.forEach((li) => {
+//     //     ulElement.style.display = "none";
+//     //     ulVisible = false;
+//     // });
+//     return liElements;
+// }
+
+// console.log(toggleDropList());
+
+// divElement.querySelectorAll(".taccc").forEach((li) => {
+//     li.addEventListener("click", function () {
+//         ulElement.style.display = "none";
+//         ulVisible = false;
+//     });
+// });
